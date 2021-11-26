b3da83549b78013e0e2cab1deb7f8ecf0b23f3fc
AshiqQups <ashiq.qups@gmail.com>
2021-11-26 21:11:29 +0600

    Atata .net Sample project

---
+++ SampleApp.UITests/UITestFixture.cs
@@ -0,0 +1,29 @@
+﻿using Atata;
+using NUnit.Framework;
+
+namespace SampleApp.UITests
+{
+    [TestFixture]
+    public class UITestFixture
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            AtataContext.Configure().Build();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            AtataContext.Current?.CleanUp();
+        }
+
+        protected UsersPage Login()
+        {
+            return Go.To<SignInPage>()
+                .Email.Set("admin@mail.com")
+                .Password.Set("abc123")
+                .SignIn.ClickAndGo();
+        }
+    }
+}
---
+++ SampleApp.UITests/UserTests.cs
@@ -0,0 +1,33 @@
+﻿using Atata;
+using NUnit.Framework;
+
+namespace SampleApp.UITests
+{
+    public class UserTests : UITestFixture
+    {
+        [Test]
+        public void Create()
+        {
+            Office office = Office.NewYork;
+            Gender gender = Gender.Male;
+
+            Login() // Returns UsersPage.
+                .New.ClickAndGo() // Returns UserEditWindow.
+                    .ModalTitle.Should.Equal("New User")
+                    .General.FirstName.SetRandom(out string firstName)
+                    .General.LastName.SetRandom(out string lastName)
+                    .General.Email.SetRandom(out string email)
+                    .General.Office.Set(office)
+                    .General.Gender.Set(gender)
+                    .Save.ClickAndGo() // Returns UsersPage.
+                .Users.Rows[x => x.Email == email].View.ClickAndGo() // Returns UserDetailsPage.
+                    .AggregateAssert(page => page
+                        .Header.Should.Equal($"{firstName} {lastName}")
+                        .Email.Should.Equal(email)
+                        .Office.Should.Equal(office)
+                        .Gender.Should.Equal(gender)
+                        .Birthday.Should.Not.Exist()
+                        .Notes.Should.Not.Exist());
+        }
+    }
+}
